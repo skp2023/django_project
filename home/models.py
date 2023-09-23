a0fa6626be8828e27266e05d7bc34743a6bb9028
skp2023 <sureshpatel2005@gmail.com>
2023-09-23 21:56:18 +0530

    add models.py and migration

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    #id = models.AutoField()
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField()
+    image = models.ImageField()
+    file = models.FileField()
+
+class product(models.Model):
+    pass
