91e67838878b2727b79ae105ba63ce6484e5d098
skp2023 <sureshpatel2005@gmail.com>
2023-09-26 12:01:50 +0530

    update button in receipe

--- core/urls.py
+++ core/urls.py
@@ -25,6 +25,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', home , name="home"),
     path('receipes/' , receipes , name="receipes"),
+    path('delete-receipe/<id>/' , delete_receipe , name="delete_receipe"),
+    path('update-receipe/<id>/' , update_receipe , name="update_receipe"),
     path('contact/', contact , name="contact"),
     path('about/', about , name="about"),
 
--- vege/views.py
+++ vege/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render , redirect
 from .models import *
-
+from django.http import HttpResponse
 # Create your views here.
 
 def receipes(request):
@@ -21,3 +21,33 @@ def receipes(request):
     queryset = Receipe.objects.all()
     context = {'receipes': queryset}
     return render(request , 'receipes.html', context)
+
+def update_receipe(request, id):
+
+    if request.method == "POST":
+        data = request.POST
+
+        receipe_image = request.FILES.get('receipe_image')
+        receipe_name = data.get('receipe_name')
+        receipe_description = data.get('receipe_description')
+
+        queryset = Receipe.objects.get(id = id)
+        queryset.receipe_name = receipe_name
+        queryset.receipe_description = receipe_description
+        
+        if receipe_image:
+            queryset.receipe_image = receipe_image
+
+        queryset.save()
+        return redirect('/receipes/')
+
+    queryset = Receipe.objects.get(id = id)
+    context = {'receipe': queryset}
+    return render(request , 'update_receipes.html', context)
+    
+
+def delete_receipe(request, id):
+    
+    queryset = Receipe.objects.get(id = id)
+    queryset.delete()
+    return redirect('/receipes/')
